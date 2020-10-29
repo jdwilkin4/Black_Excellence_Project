cd43c5add7d9fd24b4e0bcb1c16cdb5b4621af1a
Jessica Wilkins <jwilkin4@hotmail.com>
2020-10-28 21:40:00 -0700

    working on main navigation for all pages

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+//main navigation menu
+const template = document.createElement('template');
+template.innerHTML = `
+<nav class="nav-bar" id="mobile-nav">
+<a class="nav-links" href="#logo">BEP</a>
+<a class="nav-links active" href="/index.html">Home</a>
+<a class="nav-links" href="#about">About</a>
+<a class="nav-links" href="#artists">Artists</a>
+<a class="nav-links" href="#resources">Resources</a>
+<a class="nav-links" href="#contact">Contact</a>
+<a href="javascript:void(0);" class="mobile-bars" onclick="mobileNav()">
+    <i class="fas fa-bars"></i>
+</a>
+</nav> 
+`
+
+document.body.appendChild(template.content);
+
+
 // mobile nav bar toggle
 
 function mobileNav(){
