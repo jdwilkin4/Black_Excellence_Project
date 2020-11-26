5aa2b8ffae7698c2e99aa408c2d527b161a06576
Jessica Wilkins <jwilkin4@hotmail.com>
2020-11-26 03:03:30 -0800

    working on florence quiz

---
+++ Composers/Florence-Price/florence-quiz.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "What age did Florence have her first composition published?",
+        answers: {
+            a:	9,
+            b:	10,
+            c:	11,
+            d:	12
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where did Florence receive her Bachelor of Music degree?",
+        answers: {
+            a:	"New England Conservatory",
+            b:	"Julliard",
+            c:	"Eastman", 
+            d:	"Curtis" 
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What was the name of the music teacher’s association that denied Florence admission?",
+        answers: {
+            a:	"Arkansas Music Teachers Association",
+            b:	"Chicago Music Teachers Association",
+            c:	"New York Music Teachers Association",
+            d:	"Los Angeles Music Teachers Association" 
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What was the name of Florence’s second husband?",
+        answers: {
+            a:	"Randy Johnson",
+            b:	"Scott Mills",
+            c:	"Daniel Williams",
+            d:	"Pusey Dell Arnett"            
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What was the name of the orchestra that performed her first symphony in 1933?",
+        answers: {
+            a:	"New York Philharmonic",
+            b:	"Chicago Symphony",
+            c:	"Los Angeles Philharmonic",
+            d:	"San Francisco Symphony",                        
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which college did Florence NOT continue her education in? ",
+        answers: {
+            a:	"American Conservatory of Music",
+            b:	"Chicago Teachers College",
+            c:	"University of Maryland", 
+            d:	"Chicago Musical College"                         
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What does CMA stand for?",
+        answers: {
+            a:	"Charlotte Music Association",
+            b:	"Chicago Music Association",
+            c:	"Cabot Music Association",
+            d:	"Cadillac Music Association"                           
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which way did Florence NOT earn a living?",
+        answers: {
+            a:	"Opera singer",
+            b:	"Sales from piano works",
+            c:	"Orchestrator for WGN radio",
+            d:	"Organist for silent films"                          
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which conductor commissioned Florence to write a suite for strings?",
+        answers: {
+            a:	"Leonard Bernstein",
+            b:	"Pierre Boulez",
+            c:	"Herbert von Karajan",
+            d:	"Sir John Barbirolli"                            
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What year did Florence Price die from complications of a stroke?",
+        answers: {
+            a:	1950,
+            b:	1940,
+            c:	1960,
+            d:	1953                                       
+        },
+        correctAnswer: "d"
+    }
+]
