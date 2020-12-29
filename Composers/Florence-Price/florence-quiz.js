c4d58947f6eb73d77e86bff0a6ced8f1cb517513
Jessica Wilkins <jwilkin4@hotmail.com>
2020-12-28 19:01:06 -0800

    reverting back to 3036a85

--- Composers/Florence-Price/florence-quiz.js
+++ Composers/Florence-Price/florence-quiz.js
@@ -3,12 +3,115 @@ const submit = document.getElementById("submit");
 const reset = document.getElementById("reset");
 const results = document.getElementById("results");
 
-let questions = {};
 let numOfCorrectAnswers = 0;
+
+const questions = [
+    {
+        question: "1. What age did Florence have her first composition published?",
+        answers: {
+            a:	9,
+            b:	10,
+            c:	11,
+            d:	12
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "2. Where did Florence receive her Bachelor of Music degree?",
+        answers: {
+            a:	"New England Conservatory",
+            b:	"Julliard",
+            c:	"Eastman", 
+            d:	"Curtis" 
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "3. What was the name of the music teacher’s association that denied Florence admission?",
+        answers: {
+            a:	"Arkansas Music Teachers Association",
+            b:	"Chicago Music Teachers Association",
+            c:	"New York Music Teachers Association",
+            d:	"Los Angeles Music Teachers Association" 
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "4. What was the name of Florence’s second husband?",
+        answers: {
+            a:	"Randy Johnson",
+            b:	"Scott Mills",
+            c:	"Daniel Williams",
+            d:	"Pusey Dell Arnett"            
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "5. What was the name of the orchestra that performed her first symphony in 1933?",
+        answers: {
+            a:	"New York Philharmonic",
+            b:	"Chicago Symphony",
+            c:	"Los Angeles Philharmonic",
+            d:	"San Francisco Symphony",                        
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "6. Which college did Florence NOT continue her education in? ",
+        answers: {
+            a:	"American Conservatory of Music",
+            b:	"Chicago Teachers College",
+            c:	"University of Maryland", 
+            d:	"Chicago Musical College"                         
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "7. What does CMA stand for?",
+        answers: {
+            a:	"Charlotte Music Association",
+            b:	"Chicago Music Association",
+            c:	"Cabot Music Association",
+            d:	"Cadillac Music Association"                           
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "8. Which way did Florence NOT earn a living?",
+        answers: {
+            a:	"Opera singer",
+            b:	"Sales from piano works",
+            c:	"Orchestrator for WGN radio",
+            d:	"Organist for silent films"                          
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "9. Which conductor commissioned Florence to write a suite for strings?",
+        answers: {
+            a:	"Leonard Bernstein",
+            b:	"Pierre Boulez",
+            c:	"Herbert von Karajan",
+            d:	"Sir John Barbirolli"                            
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "10. What year did Florence Price die from complications of a stroke?",
+        answers: {
+            a:	1950,
+            b:	1940,
+            c:	1960,
+            d:	1953                                       
+        },
+        correctAnswer: "d"
+    }
+]
 
 let createQuiz = () => {
     
     const quizContainer = [];
+
     questions.forEach(
         (currentQuestion, questionNum) => {
             
@@ -32,9 +135,11 @@ let createQuiz = () => {
 
     quiz.innerHTML = quizContainer.join('');
 }
+createQuiz(); 
 
 let displayResults = () => {
     const answerContainer = quiz.querySelectorAll('.answers');
+
     questions.forEach((currentQuestion, questionNum) => {
         const collectionOfAnswers = answerContainer[questionNum];
         const checkedInput = `input[name=question${questionNum}]:checked`;
@@ -58,28 +163,6 @@ let resetQuiz = () => {
     window.scrollTo(0, 0);
 }
 
-const loadJSON = (callback) => { 
-    const xobj = new XMLHttpRequest(); 
-     xobj.overrideMimeType("application/json"); 
-     xobj.open('GET', 'florence-quiz-questions.json', true);  
-     xobj.onreadystatechange =  () => { 
-        if (xobj.readyState == 4 && xobj.status == "200") { 
-        callback(xobj.responseText); 
-        } 
-     }; 
-     xobj.send(null);  
-} 
-
-loadJSON()
-  .then((questionsJson) => {
-    questions = questionsJson;
-    submit.addEventListener('click', displayResults);
-    reset.addEventListener('click', resetQuiz);
-    createQuiz();
-  })
-  .catch(alert);
-   
-
-
-
+submit.addEventListener('click', displayResults);
+reset.addEventListener('click', resetQuiz);
 
