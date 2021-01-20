6fd409db4521ba54ab9f4be4091562f69a1c6cc9
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-20 02:25:15 -0800

    creating a day in the life game files

---
+++ Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.js
@@ -0,0 +1,27 @@
+let message = document.getElementById('message');
+const tour = document.getElementById('tour');
+const gigs = document.getElementById('gigs');
+const publishing = document.getElementById('publishing');
+const education = document.getElementById('education');
+
+//initialize the buttons with onclick functions
+tour.onclick = goOnTour;
+gigs.onclick = getGigs;
+publishing.onclick = publish;
+education.onclick = getEducated;
+
+const goOnTour = () => {
+
+}
+
+const getGigs = () => {
+
+}
+
+const publish = () => {
+
+}
+
+const getEducated = () => {
+    
+}
