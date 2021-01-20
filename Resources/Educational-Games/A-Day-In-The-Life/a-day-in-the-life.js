7ec6996a68a01bcdb26708b209169f9835e12947
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-20 03:53:24 -0800

    building on game functions

--- Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.js
+++ Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.js
@@ -1,8 +1,11 @@
+let money = 300;
+let imgs = document.getElementById('imgs');
 let message = document.getElementById('message');
 const tour = document.getElementById('tour');
 const gigs = document.getElementById('gigs');
 const publishing = document.getElementById('publishing');
 const education = document.getElementById('education');
+
 
 //initialize the buttons with onclick functions
 tour.onclick = goOnTour;
@@ -10,12 +13,22 @@ gigs.onclick = getGigs;
 publishing.onclick = publish;
 education.onclick = getEducated;
 
-const goOnTour = () => {
+const locations = [
+    {
+        name:'Grand Concert Hall',
+        'button text':['Rehearse', 'Perform', 'Negotiate Rates', 'Go Home'],
+        'button functions' : [rehearse, perform, negotiate, home],
+        text:'Welcome to Grand Concert Hall'
+    }
+]
 
+
+const goOnTour = () => {
+    
 }
 
 const getGigs = () => {
-
+   message.innerText = "hi"
 }
 
 const publish = () => {
@@ -24,4 +37,21 @@ const publish = () => {
 
 const getEducated = () => {
     
+}
+
+//concert hall functions
+const rehearse = () => {
+
+}
+
+const perform = () => {
+
+}
+
+const negotiate = () => {
+
+}
+
+const home = () => {
+
 }
