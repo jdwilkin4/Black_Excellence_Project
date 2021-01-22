b2ae2ff4eb66960494fd37628ee5d844d39d420c
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-21 17:21:59 -0800

    adding images for a day in the life game

--- Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.js
+++ Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.js
@@ -1,22 +1,13 @@
 const message = document.getElementById("message");
-const earnings = document.getElementById("earnings")
-let weeks = 0
-let money = 0
+const earnings = document.getElementById("earnings");
+const img = document.getElementById("img");
 const home = document.getElementById("home");
 const button1 = document.getElementById("btn1");
 const button2  = document.getElementById("btn2");
 const button3 = document.getElementById("btn3");
 const button4 = document.getElementById("btn4");
-
-
-/* const locations = [
-    {
-        name:'Grand Concert Hall',
-        'button text':['Rehearse', 'Perform', 'Negotiate Rates', 'Go Home'],
-        'button functions' : [rehearse, perform, negotiate, home],
-        text:'Welcome to Grand Concert Hall'
-    }
-] */
+let weeks = 0;
+let money = 0;
 
 //onclick options for main buttons
 home.onclick = goHome;
@@ -25,10 +16,52 @@ button2.onclick = getGigs;
 button3.onclick = teaching;
 button4.onclick = getEducated;
 
+//function for smooth transitions 
+const fadeIn = (el, time) => {
+    el.style.opacity = 0;
+    el.style.display = "block";
+  
+    let last = +new Date();
+
+    let tick = () => {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+      last = +new Date();
+  
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      }
+    };
+  
+    tick();
+}
+
+//fisher yates shuffle for the array responses
+function shuffle(array) {
+    let remainingElements = array.length, temp, i;
+  
+    // While there remain elements to shuffle…
+    while (remainingElements) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * remainingElements--);
+  
+      // And swap it with the current element.
+      temp = array[remainingElements];
+      array[remainingElements] = array[i];
+      array[i] = temp;
+    }
+  
+    return array;
+}
+  
+  
+
 //home button
 function goHome() {
     message.innerHTML = "Welcome home";
-    earnings.innerHTML = `Total earnings after week ${weeks}: $ ${money}`
+    earnings.innerHTML = `Total earnings after week ${weeks}: $ ${money}`;
+    img.setAttribute("src", "/Resources/Educational-Games/A-Day-In-The-Life/images/new-york-cityscape.jpg");
+    fadeIn(img,1000);
     button1.innerHTML = "Go on Tour";
     button2.innerHTML = "Local gigs";
     button3.innerHTML = "Teaching";
@@ -114,56 +147,66 @@ function arizona() {
 
 }
 
-
-
+//Midwest options
 function midwest() {
     message.innerHTML = "Pick a state";
 
 }
 
+// south options
 function south() {
     message.innerHTML = "Pick a state";
 
 }
 
+//east coast performance options
 function eastCoast() {
     message.innerHTML = "Pick a state";
 
 }
 
+//local gigs option
 function getGigs() {
 
 }
 
+//teaching function
 function teaching() {
     let teachingResponses = [
         "You were sick and didn't teach this week", 
         "You made $5 this week teaching", 
         "You made $10 teaching this week"
     ]
+
+    shuffle(teachingResponses);
+
     let randomNum = Math.floor(Math.random() * teachingResponses.length);
     let randomMsg = teachingResponses[randomNum];
 
-    if(randomMsg === teachingResponses[0]){
-        message.innerHTML = teachingResponses[0]
+    if(randomMsg === "You were sick and didn't teach this week"){
+        message.innerHTML = randomMsg
         weeks++
         earnings.innerHTML = `Total earnings after week ${weeks}: $ ${money}`
-    }else if(randomMsg === teachingResponses[1]){
-        message.innerHTML = teachingResponses[1]
+    }else if(randomMsg === "You made $5 this week teaching"){
+        message.innerHTML = randomMsg
         weeks++
         money += 5
         earnings.innerHTML = `Total earnings after week ${weeks}: $ ${money}`
     }else {
-        message.innerHTML = teachingResponses[2]
+        message.innerHTML = randomMsg
         weeks++
         money += 10
         earnings.innerHTML = `Total earnings after week ${weeks}: $ ${money}`
     }
+    img.setAttribute("src", "/Resources/Educational-Games/A-Day-In-The-Life/images/piano.jpg");
+    fadeIn(img,1000);
 }
 
-
+//higher ed function
 function getEducated() {
-    message.innerHTML = "You were denied acceptance to the conservatory because of your color"
+    message.innerHTML = "You were denied acceptance to the conservatory because you are black";
+    img.setAttribute("src", "/Resources/Educational-Games/A-Day-In-The-Life/images/music-hall1.jpg");
+    fadeIn(img,1000);
 }
 
 
