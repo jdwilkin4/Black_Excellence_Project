8e03964ee486bd9f8314f4a408411fe5d542bd9c
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-22 17:21:23 -0800

    reorganizing quiz files and cleaning up resources page

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //main navigation menu
 document.getElementById("navMenu").innerHTML = `
 <nav class="nav-bar" id="mobile-nav">
-<a class="nav-links" href="#logo">BEMP</a>
-<a class="nav-links active" href="/index.html">Home</a>
-<a class="nav-links" href="#about">About</a>
-<a class="nav-links" href="/Composers/composers.html">Composers</a>
-<a class="nav-links" href="/Resources/resources.html">Resources</a>
-<a class="nav-links" href="#contact">Contact</a>
-<a href="javascript:void(0);" class="mobile-bars" onclick="mobileNav()">
-    <i class="fas fa-bars"></i>
-</a>
+    <a class="nav-links" href="#logo">BEMP</a>
+    <a class="nav-links active" href="/index.html">Home</a>
+    <a class="nav-links" href="#about">About</a>
+    <a class="nav-links" href="/Composers/composers.html">Composers</a>
+    <a class="nav-links" href="/Resources/resources.html">Resources</a>
+    <a class="nav-links" href="#contact">Contact</a>
+    <a href="javascript:void(0);" class="mobile-bars" onclick="mobileNav()">
+        <i class="fas fa-bars"></i>
+    </a>
 </nav> 
 `;
 
