98fa5cec287e2f68e60eb6c643ef019245d5b779
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-22 18:48:17 -0800

    creating about and contact pages. adding footer

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ document.getElementById("navMenu").innerHTML = `
 <nav class="nav-bar" id="mobile-nav">
     <a class="nav-links" href="#logo">BEMP</a>
     <a class="nav-links active" href="/index.html">Home</a>
-    <a class="nav-links" href="#about">About</a>
+    <a class="nav-links" href="/about.html">About</a>
     <a class="nav-links" href="/Composers/composers.html">Composers</a>
     <a class="nav-links" href="/Resources/resources.html">Resources</a>
-    <a class="nav-links" href="#contact">Contact</a>
+    <a class="nav-links" href="/contact.html">Contact</a>
     <a href="javascript:void(0);" class="mobile-bars" onclick="mobileNav()">
         <i class="fas fa-bars"></i>
     </a>
@@ -25,3 +25,19 @@ function mobileNav(){
     }
 
 }
+
+//footer
+document.getElementById("footer").innerHTML = `
+    <footer class="footer-container">
+        <div class="footer-styles">
+            <a class="footer-links" href="/about.html">About Us</a>
+            <a class="footer-links" href="https://www.youtube.com/watch?v=FIWELCEBG0E&list=PLqHdMZJ08WM11cAx7YqdmvHb5YawvM0sq" target="_blank">Black Excellence Youtube Series</a>
+        </div>
+        
+        <div class="footer-styles">
+            <a class="footer-links" href="/Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.html">A day in the Life Game</a>
+            <a class="footer-links" href="/Resources/Educational-Games/Memory-Game/Memory-game.html">Black Excellence Memory Game</a>
+        </div>
+        <a class="footer-links" href="/contact.html">Contact Us</a>
+    </footer>
+`
