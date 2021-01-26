ea3907821d413edf4e6dcedfb980933c0f9cbfef
nhcarrigan <nhcarrigan@gmail.com>
2021-01-25 18:32:55 -0800

    feat: modularise nav bar functions
    Creates modular functions in `nav.js` for maintainability and scaling.
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- nav.js
+++ nav.js
@@ -9,23 +9,34 @@ document.getElementById("navMenu").innerHTML = `
     <a class="nav-links" id='contact-link' href="/contact.html">Contact</a>
 </nav> 
 `;
+/**
+ * Sets the active class on the nav link with an id matching
+ * the given string
+ * @param {String} fileName ID name to find (followed by `-link`) 
+ */
+const setActive = (fileName) => {
+  const activeLink = document.getElementById(`${fileName}-link`);
+  activeLink.classList.add("active");
+};
 
-const determineNav = (string) => {
-    console.log(string);
-    if (string.includes('Composers')) {
-        const activeLink = document.getElementById('composers-link');
-        activeLink.classList.add('active');
-    }
-    if (string.includes('Resources')) {
-        const activeLink = document.getElementById('resources-link');
-        activeLink.classList.add('active');
-    }
-}
+/**
+ * 
+ * @param {String} filePath
+ */
+const determineNav = (filePath) => {
+  console.log(filePath);
+  if (filePath.includes("Composers")) {
+    setActive('composers')
+  }
+  if (filePath.includes("Resources")) {
+    setActive('resources')
+  }
+};
 
 (() => {
-    const location = window.location.pathname;
-    const fileName = location.split('/').reverse()[0];
-    const fileNameNoExtension = fileName.replace('.html', '');
-    const activeLink = document.getElementById(`${fileNameNoExtension}-link`);
-    activeLink ? activeLink.classList.add('active') : determineNav(location);
+  const location = window.location.pathname;
+  const fileName = location.split("/").reverse()[0];
+  const fileNameNoExtension = fileName.replace(".html", "");
+  const activeLink = document.getElementById(`${fileNameNoExtension}-link`);
+  activeLink ? activeLink.classList.add("active") : determineNav(location);
 })();
