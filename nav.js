77013f4f26407af690abaa94c60e4e505a3a599e
nhcarrigan <nhcarrigan@gmail.com>
2021-01-25 18:21:58 -0800

    feat: dynamic nav highlighting
    Nav link for active page is now highlighted.
    
    TODO: Nested subpages
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- nav.js
+++ nav.js
@@ -2,12 +2,18 @@
 document.getElementById("navMenu").innerHTML = `
 <nav class="nav-bar" id="mobile-nav">
     <a class="nav-links" href="#logo">BEMP</a>
-    <a class="nav-links active" href="/index.html">Home</a>
-    <a class="nav-links" href="/about.html">About</a>
-    <a class="nav-links" href="/Composers/composers.html">Composers</a>
-    <a class="nav-links" href="/Resources/resources.html">Resources</a>
-    <a class="nav-links" href="/contact.html">Contact</a>
+    <a class="nav-links" id='index-link' href="/index.html">Home</a>
+    <a class="nav-links" id='about-link' href="/about.html">About</a>
+    <a class="nav-links" id='composers-link' href="/Composers/composers.html">Composers</a>
+    <a class="nav-links" id='resources-link' href="/Resources/resources.html">Resources</a>
+    <a class="nav-links" id='contact-link' href="/contact.html">Contact</a>
 </nav> 
 `;
 
-
+(() => {
+    const location = window.location.pathname;
+    const fileName = location.split('/').reverse()[0];
+    const fileNameNoExtension = fileName.replace('.html', '');
+    const activeLink = document.getElementById(`${fileNameNoExtension}-link`);
+    activeLink.classList.add('active');
+})();
