4a60dc4aa9728e3a84e17587c382dbea71f8c741
nhcarrigan <nhcarrigan@gmail.com>
2021-01-25 18:27:38 -0800

    feat: nav handles nested pages
    Adds a helper function to detect the appropriate nav link for a nested
    html page.
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- nav.js
+++ nav.js
@@ -10,10 +10,22 @@ document.getElementById("navMenu").innerHTML = `
 </nav> 
 `;
 
+const determineNav = (string) => {
+    console.log(string);
+    if (string.includes('Composers')) {
+        const activeLink = document.getElementById('composers-link');
+        activeLink.classList.add('active');
+    }
+    if (string.includes('Resources')) {
+        const activeLink = document.getElementById('resources-link');
+        activeLink.classList.add('active');
+    }
+}
+
 (() => {
     const location = window.location.pathname;
     const fileName = location.split('/').reverse()[0];
     const fileNameNoExtension = fileName.replace('.html', '');
     const activeLink = document.getElementById(`${fileNameNoExtension}-link`);
-    activeLink.classList.add('active');
+    activeLink ? activeLink.classList.add('active') : determineNav(location);
 })();
