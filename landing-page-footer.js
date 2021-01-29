bdab970b367a8d2337b8928d56fe49c63aecd085
Jessica Wilkins <jwilkin4@hotmail.com>
2021-01-28 19:41:42 -0800

    fixing issues with path for live site

--- landing-page-footer.js
+++ landing-page-footer.js
@@ -2,9 +2,9 @@ document.getElementById('footer').innerHTML = `
     <footer class="footer-container">
             <a class="footer-links" href="about.html">About Us</a>
             <a class="footer-links" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=FIWELCEBG0E&list=PLqHdMZJ08WM11cAx7YqdmvHb5YawvM0sq" target="_blank">Black Excellence Series</a>
-            <a class="footer-links" href="/Resources/Educational-Games/A-Day-In-The-Life/a-day-in-the-life.html">A day in the Life Game</a>
-            <a class="footer-links" href="/Resources/Educational-Games/Memory-Game/Memory-game.html">Memory Game</a>
-            <a class="footer-links" href="/Resources/Educational-Games/Quizzes/duke-quiz.html">Duke Ellington Quiz</a>
-            <a class="footer-links" href="/Resources/Educational-Games/Quizzes/eleanor-quiz.html">Eleanor Alberga Quiz</a>
+            <a class="footer-links" href="Educational-Games/A-Day-In-The-Life/a-day-in-the-life.html">A day in the Life Game</a>
+            <a class="footer-links" href="Educational-Games/Memory-Game/Memory-game.html">Memory Game</a>
+            <a class="footer-links" href="Educational-Games/Quizzes/duke-quiz.html">Duke Ellington Quiz</a>
+            <a class="footer-links" href="Educational-Games/Quizzes/eleanor-quiz.html">Eleanor Alberga Quiz</a>
     </footer>
 `;
